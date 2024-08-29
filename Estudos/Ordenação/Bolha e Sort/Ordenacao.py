766059eae45f59dc7e0a2cfb3394c813e9634c59
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2024-08-29 15:36:33 -0300

    Update Ordenacao.py

--- Estudos/Ordenação/Bolha e Sort/Ordenacao.py
+++ Estudos/Ordenação/Bolha e Sort/Ordenacao.py
@@ -21,3 +21,16 @@ class Ordenacao:
                 lista[j+1] = lista[j]
             
             lista[j+1] = aux #momento da insercao
+
+    @staticmethod
+    def selecao(lista):       
+        for i in range(0, len(lista) -1):
+            posMenor = i
+            for j in range(i+1, len(lista)):
+                if (lista[j] < lista[posMenor]):
+                    posMenor = j
+                
+            if (i != posMenor):
+                tmp = lista[i]
+                lista[i] = lista[posMenor]
+                lista[posMenor] = tmp
